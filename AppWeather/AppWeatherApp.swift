ef4448c8072d95258d1e89999fd4c30545abee5d
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2025-05-07 15:47:23 +0200

    Initial Commit

---
+++ AppWeather/AppWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWeatherApp.swift
+//  AppWeather
+//
+//  Created by Krzysztof Zaporowski on 07/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
